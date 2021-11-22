e3242d7a9e9ac79dad67eef46da24b55c90b04d7
Natybet29 <92266341+Natybet29@users.noreply.github.com>
2021-11-22 16:28:32 -0600

    4.28

--- Assets/Scripts/DarlaController.cs
+++ Assets/Scripts/DarlaController.cs
@@ -4,19 +4,38 @@ using UnityEngine.UI;
 
 public class DarlaController : MonoBehaviour
 {
-    public float gunSpeed = 1;
-    public GameObject SnowBall;
+
     public float Health = 100f;
     Rigidbody rig;
     public float speed = 1;
     public float jumpForce = 1;
+    public AudioClip damageSound;
+    AudioSource audioSource;
 
+    [Header("Player Setting")]
+       
+        public bool stopMoverment = false;
+        public bool moving { get; set; }
+        
+        float m_Horizontal, m_Vertical;
+        private Vector3 m_MoveVector;
+         
+    [HideInInspector]
+        public Animator m_Animator;
+        private Quaternion m_Rotation = Quaternion.identity;
+        private Transform camTrans;
+        private Vector3 camForward;
+
+        private Vector3 offset;  
 
     // Start is called before the first frame update
     void Start()
     {   
         
         rig = GetComponent<Rigidbody>();
+        audioSource= GetComponent<AudioSource>();
+        m_Animator = GetComponent<Animator>();     
+        camTrans = Camera.main.transform;  
     }
 
     // Update is called once per frame
@@ -24,7 +43,7 @@ public class DarlaController : MonoBehaviour
     {
          if (Input.GetMouseButtonDown(0))
         {
-            Shoot();
+          
         }
         
         float vertical = Input.GetAxis("Vertical");
@@ -38,14 +57,84 @@ public class DarlaController : MonoBehaviour
 
         }
 
-        
-    }  
-    void Shoot()
+ 
+    }
+     void FixedUpdate()
     {
-        GameObject newSnowBall = Instantiate( SnowBall, tranform.position + tranform.forward, new Quaternion( 0,0,0,0));
-        Rigidbody snowBallRig = newSnowBall.tranform.GetComponent<Rigidbody>();
-        snowBallRig.velocity = transform.forward * gunSpeed;
+           // move vector 
+            if (camTrans != null)
+            {
+                camForward = Vector3.Scale(camTrans.forward, new Vector3(1, 0, 1).normalized);
+                m_MoveVector = m_Vertical * camForward + m_Horizontal * camTrans.right;
+                m_MoveVector.Normalize();
+            }
+            //animation    
+            bool has_H_Input = !Mathf.Approximately(m_Horizontal, 0);
+            bool has_V_Input = !Mathf.Approximately(m_Vertical, 0);
 
-        Destroy(newSnowBall, 5);
+            if (!stopMoverment) moving = has_H_Input || has_V_Input;
+            else moving = false;
+
+            float inputSpeed = Mathf.Clamp01( Mathf.Abs(m_Horizontal) + Mathf.Abs(m_Vertical));
+
+            m_Animator.SetBool(Const.Moving, moving);
+            m_Animator.SetFloat(Const.Speed, inputSpeed);
+
+            //move and rotate
+            if (moving)
+            {
+                Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_MoveVector, turnSpeed * Time.deltaTime, 0f);
+                m_Rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredForward), turnSpeed);
+                m_Rigidbody.MoveRotation(m_Rotation);
+                m_Rigidbody.MovePosition(m_Rigidbody.position + inputSpeed*m_MoveVector * runSpeed * Time.deltaTime);
+            }
+    }
+    public void Hit()
+    { 
+        Health=Health -1;
+        if (Health == 0) Destroy( GameObject);
+        audioSource.clip= damageSound;
+        audioSource.play();
+    }
+    public void HitSpider()
+    { 
+        Health=Health -3;
+        if (Health == 0) Destroy( GameObject);
+        audioSource.clip= damageSound;
+        audioSource.play();
+    }
+     public void HitMonster()
+    { 
+        Health=Health -3;
+        if (Health == 0) Destroy( GameObject);
+        audioSource.clip= damageSound;
+        audioSource.play();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.compareTag("Spider"))
+        {
+            HitSpider();
+        }
+        else if (collision.gameObject.compareTag("Monster"))
+        {
+            HitMonster();
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        IBox box = other.GetComponent<IBox>();
+        if (box != null)
+        {
+            int res = box.OpenBox();
+            if (box.getID() == (int)BoxID.HEALT)
+            {
+                Health += res;
+              
+            }
+            Destroy(other.gameObject);
+        }
+
+
     }
 }
